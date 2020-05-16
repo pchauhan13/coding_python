d1462e655821cafad38c2b801b3ea60f3b22d36d
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-16 17:14:15 +0530

    program to use basic math operations over number

---
+++ program_5.py
@@ -0,0 +1,14 @@
+print(2)
+print(2.8423)
+print(3 + 4)
+print(3 + 4.5)
+
+my_num = 5
+print(str(my_num) + " my favorite number")
+
+my_num = -5
+print(abs(my_num))
+print(pow(5, 3))
+print(max(5, 3))
+print(min(5, 3))
+print(round(3.4))
