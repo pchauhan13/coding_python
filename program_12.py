35bb27940502d0ed77e048028a602b6afeece83b
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-17 09:32:43 +0530

    program to use if..elif..else statements

---
+++ program_12.py
@@ -0,0 +1,20 @@
+is_male = True
+is_tall = True
+
+if is_male or is_tall:
+    print("You are a male or tall or both.")
+else:
+    print("You are neither male nor tall.")
+
+if is_male and is_tall:
+    print("You are a tall male.")
+elif is_male and not(is_tall):
+    print("You are a short male.")
+elif not(is_male) and is_tall:
+    print("You are not a male but are tall")
+elif is_male:
+    print("You are a male.")
+elif is_tall:
+    print("You are tall.")
+else:
+    print("You are either not male or not tall.")
