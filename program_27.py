67439b26e3dd1354f709a713a12f3b89d2f3d5ad
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-17 12:11:09 +0530

    adding arguments to the class

--- program_27.py
+++ program_27.py
@@ -5,5 +5,12 @@ class Student:
         self.gpa = gpa
         self.is_on_probation = is_on_probation
 
+    def on_honor_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False
+
 student1 = Student("Jim", "Business", 3.15, False)
 print(student1)
+print(student1.on_honor_roll())
