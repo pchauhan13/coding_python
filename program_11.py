2b8b83ecc7d7c51781934decce9ba4b48c16bca2
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-05-17 09:27:48 +0530

    program to create and call functions with arguments and using the return values

---
+++ program_11.py
@@ -0,0 +1,14 @@
+def sayHi():
+    print("Hello user!")
+
+sayHi()
+
+def sayHiName(name):
+    print("Hello " + name)
+
+sayHiName("Prashant")
+
+def cube(num):
+    return num * num * num
+
+print("Cube of 3:", cube(3))
