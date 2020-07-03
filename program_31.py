7ec08daf6b0ce17fbae3274d68b6f0d09e592a7d
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-07-03 08:37:35 +0530

    using filter with None to remove non-true values from the list

--- program_31.py
+++ program_31.py
@@ -1,2 +1,5 @@
 creature_names = ['Sammy', 'Ashley', 'Jo', 'Olly', 'Jackie', 'Charlie']
 print(list(filter(lambda x: x[0].lower() in 'aeiou', creature_names)))
+
+aquarium_tanks = [11, False, 18, 21, "", 12, 34, 0, [], {}]
+print(list(filter(None, aquarium_tanks)))
