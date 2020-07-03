c2bd82ab74b2ae86d72a69b30c07f102b8ee27ce
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-07-03 08:25:24 +0530

    program to use thredpoolexecutor for running a function in threads based execution

--- program_30.py
+++ program_30.py
@@ -1,4 +1,5 @@
 import requests
+import concurrent.futures
 
 def get_wiki_page_existence(wiki_page_url, timeout=10):
     response = requests.get(wiki_page_url, timeout=timeout)
@@ -12,5 +13,19 @@ def get_wiki_page_existence(wiki_page_url, timeout=10):
     return wiki_page_url + " - " + page_status
 
 
-url = "https://en.wikipedia.org/wiki/Ocean"
-print(get_wiki_page_existence(url))
+# url = "https://en.wikipedia.org/wiki/Ocean"
+# print(get_wiki_page_existence(url))
+
+wiki_page_urls = [
+    "https://en.wikipedia.org/wiki/Ocean",
+    "https://en.wikipedia.org/wiki/Island",
+    "https://en.wikipedia.org/wiki/this_page_does_not_exist",
+    "https://en.wikipedia.org/wiki/Shark",
+]
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = []
+    for url in wiki_page_urls:
+        futures.append(executor.submit(get_wiki_page_existence, wiki_page_url=url))
+    for future in concurrent.futures.as_completed(futures):
+        print(future.result())
